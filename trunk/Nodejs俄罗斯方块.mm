312bf834fe4a0cc9e9e1888ddfd9b2c8b0729949
zhuifeng1017@gmail.com <zhuifeng1017@gmail.com@07e9d9db-e16a-e3d7-0fbb-d06a655cebc4>
2012-11-22 03:46:16 +0000

    0000000
    git-svn-id: http://nodejs-tetris.googlecode.com/svn@2 07e9d9db-e16a-e3d7-0fbb-d06a655cebc4

---
+++ trunk/Nodejs俄罗斯方块.mm
@@ -0,0 +1,72 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1282109751875" ID="ID_740566840" MODIFIED="1333274554703" TEXT="&#x4fc4;&#x7f57;&#x65af;&#x65b9;&#x5757;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">&#20851;&#38190;&#28857;&#65306;&#23558;&#27963;&#21160;&#21306;&#22495;&#21010;&#20998;&#20026;10*X&#30340;&#23567;&#26041;&#26684;&#65292;&#22312;&#32593;&#39029;&#20013;&#29992;&#26631;&#31614;&lt;span&gt;&#34920;&#31034;&#12290; </font>
+    </p>
+    <p>
+      <font size="4">&#27599;&#27425;&#25481;&#33853;&#19979;&#30340;&#26041;&#22359;&#35774;&#20026;</font><font color="#ff3333" size="4"><b>&#21487;&#35265;</b></font><font size="4">&#65292;&#20854;&#20182;&#35774;&#20026;</font><font color="#ff3333" size="4"><b>&#38544;&#34255;</b></font>&#12290;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1331690569637" HGAP="3" ID="ID_1312467438" MODIFIED="1333177108609" POSITION="left" TEXT="&#x6d41;&#x7a0b;" VSHIFT="-14">
+<node CREATED="1331690577824" HGAP="11" ID="ID_1284453507" MODIFIED="1333177080656" TEXT="&#x7528;&#x6237;&#x767b;&#x9646;&#x540e;&#xff0c;&#x7531;&#x670d;&#x52a1;&#x5668;&#x5206;&#x914d;&#x53e6;&#x4e00;&#x4e2a;&#x7528;&#x6237;&#x8fdb;&#x884c;&#x914d;&#x5bf9;&#xff0c;&#x53cc;&#x65b9;&#x90fd;OK&#x540e;&#x6e38;&#x620f;&#x5f00;&#x59cb;" VSHIFT="-17"/>
+<node CREATED="1333177041359" HGAP="16" ID="ID_264572094" MODIFIED="1333177084156" TEXT="&#x6ca1;&#x6709;&#x914d;&#x5bf9;&#x7528;&#x6237;&#x65f6;&#x7b49;&#x5f85;&#xff0c;&#x9700;&#x8981;&#x589e;&#x52a0;&#x65b0;&#x7684;&#x534f;&#x8bae;" VSHIFT="12"/>
+</node>
+<node CREATED="1331631308687" FOLDED="true" HGAP="11" ID="ID_1751650135" MODIFIED="1333274554406" POSITION="left" TEXT="&#x804a;&#x5929;&#x6570;&#x636e;&#x4e0e;&#x65b9;&#x5757;&#x6570;&#x636e;&#x5206;&#x79bb;" VSHIFT="-4">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      type&#65306;
+    </p>
+    <p>
+      &#32842;&#22825; &quot;text&quot;
+    </p>
+    <p>
+      &#26041;&#22359; &quot;spans&quot;
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1331631235437" HGAP="17" ID="ID_1913748791" MODIFIED="1333177338296" TEXT="&#x65b9;&#x5757;&#x6570;&#x636e;&#x4e0d;&#x5e7f;&#x64ad;&#xff0c;&#x53ea;&#x5bf9;&#x4f19;&#x4f34;&#x53d1;&#x9001;" VSHIFT="-10">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      session&#22686;&#21152;&#23646;&#24615;&#65292;&#35760;&#24405;&#20249;&#20276;ID&#21644;&#33258;&#24049;&#26368;&#21518;&#19968;&#24103;&#25968;&#25454;&#12290;
+    </p>
+    <p>
+      &#160;&#160;var session = {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;nick: nick,
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;id: Math.floor(Math.random()*99999999999).toString(),
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;timestamp: new Date(),
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;<font color="#ff0066">partner: &quot;&quot;,//&#20249;&#20276;id&#25110;nick</font>
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;<font color="#ff0066">lastspan:[],//&#33258;&#24049;&#26368;&#21518;&#19968;&#24103;&#25968;&#25454;</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1331631247421" HGAP="18" ID="ID_466613483" MODIFIED="1333177339421" TEXT="&#x804a;&#x5929;&#x6570;&#x636e;&#x9700;&#x5e7f;&#x64ad;&#xff0c;&#x670d;&#x52a1;&#x7aef;&#x6700;&#x591a;&#x4fdd;&#x5b58;200&#x6761;&#x6570;&#x636e;&#xff0c;&#x540e;&#x671f;&#x4e5f;&#x53ef;&#x4ee5;&#x5b9e;&#x73b0;&#x79c1;&#x804a;" VSHIFT="16"/>
+</node>
+</node>
+</map>
---
+++ trunk/server.js
@@ -0,0 +1,233 @@
+HOST = "192.168.108.1"; // localhost
+PORT = 8001;
+
+// when the daemon started
+var starttime = (new Date()).getTime();
+
+var mem = process.memoryUsage();
+// every 10 seconds poll for the memory.
+setInterval(function () {
+  mem = process.memoryUsage();
+}, 10*1000);
+
+
+var fu = require("./fu"),
+    sys = require("sys"),
+    url = require("url"),
+    qs = require("querystring");
+
+var MESSAGE_BACKLOG = 200,
+    SESSION_TIMEOUT = 60 * 1000; //session过期时间，60秒
+
+var channel = new function() {
+    var messages = [], spans = [], callbacks = [];
+    
+    this.appendMessage = function(nick, type, text) {
+        // 消息格式，使用json
+        var m = { nick: nick
+            , type: type // "msg", "join", "part"
+            , text: text
+            , timestamp: (new Date()).getTime()
+        };
+
+        switch (type) {
+            case "msg":
+                sys.puts("<" + nick + "> " + text);
+                break;
+            case "join":
+                sys.puts(nick + " join");
+                break;
+            case "part":
+                sys.puts(nick + " part");
+                break;
+        }
+
+        // 放入messages数组
+        messages.push(m);
+
+        while (callbacks.length > 0) {
+            callbacks.shift().callback([m]);
+        }
+
+        //shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+        while (messages.length > MESSAGE_BACKLOG)
+            messages.shift();
+    };
+
+    //   查询消息
+    this.query = function(since, callback) {
+        var matching = [];
+        for (var i = 0; i < messages.length; i++) {
+            var message = messages[i];
+            if (message.timestamp > since)
+                matching.push(message)
+        }
+
+        if (matching.length != 0) {
+            callback(matching);
+        } else {
+            callbacks.push({ timestamp: new Date(), callback: callback });
+        }
+    };
+
+    // clear old callbacks
+    // they can hang around for at most 30 seconds.
+    setInterval(function() {
+        var now = new Date();
+        while (callbacks.length > 0 && now - callbacks[0].timestamp > 30 * 1000) {
+            callbacks.shift().callback([]);
+        }
+    }, 3000);
+};
+
+var sessions = {};
+
+function createSession (nick) {
+  if (nick.length > 50) return null;
+  if (/[^\w_\-^!]/.exec(nick)) return null;
+
+  for (var i in sessions) {// 判断是否已存在
+    var session = sessions[i];
+    if (session && session.nick === nick) return null;
+  }
+
+  var session = { 
+    nick: nick, 
+    id: Math.floor(Math.random()*99999999999).toString(),
+    timestamp: new Date(),
+
+    poke: function () {
+      session.timestamp = new Date();
+    },
+
+    destroy: function () {
+      channel.appendMessage(session.nick, "part");
+      delete sessions[session.id];
+    }
+  };
+
+  sessions[session.id] = session; // 保存session
+  return session;
+}
+
+// interval to kill off old sessions
+setInterval(function () {
+  var now = new Date();
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id)) continue;
+    var session = sessions[id];
+
+    if (now - session.timestamp > SESSION_TIMEOUT) {
+      session.destroy();
+    }
+  }
+}, 1000);
+
+// 开始监听
+fu.listen(Number(process.env.PORT || PORT), HOST);
+
+fu.get("/", fu.staticHandler("index.html"));
+fu.get("/style.css", fu.staticHandler("style.css"));
+fu.get("/client.js", fu.staticHandler("client.js"));
+fu.get("/jquery-1.2.6.min.js", fu.staticHandler("jquery-1.2.6.min.js"));
+
+fu.get("/tt", fu.staticHandler("Tetris.html"));
+fu.get("/t_client.js", fu.staticHandler("t_client.js"));
+
+
+
+
+fu.get("/who", function (req, res) {
+  var nicks = [];
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id)) continue;
+    var session = sessions[id];
+    nicks.push(session.nick);
+  }
+  res.simpleJSON(200, { nicks: nicks
+                      , rss: mem.rss
+                      });
+});
+
+fu.get("/join", function (req, res) {
+  var nick = qs.parse(url.parse(req.url).query).nick;
+  if (nick == null || nick.length == 0) {
+    res.simpleJSON(400, {error: "Bad nick."});
+    return;
+  }
+  var session = createSession(nick);
+  if (session == null) {
+    res.simpleJSON(400, {error: "Nick in use"});
+    return;
+  }
+
+  //sys.puts("connection: " + nick + "@" + res.connection.remoteAddress);
+
+  channel.appendMessage(session.nick, "join");
+  res.simpleJSON(200, { id: session.id
+                      , nick: session.nick
+                      , rss: mem.rss
+                      , starttime: starttime
+                      });
+});
+
+fu.get("/part", function (req, res) {
+  var id = qs.parse(url.parse(req.url).query).id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.destroy();
+  }
+  res.simpleJSON(200, { rss: mem.rss });
+});
+
+fu.get("/recv", function (req, res) {
+  if (!qs.parse(url.parse(req.url).query).since) {
+    res.simpleJSON(400, { error: "Must supply since parameter" });
+    return;
+  }
+  var id = qs.parse(url.parse(req.url).query).id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.poke();
+  }
+
+  var since = parseInt(qs.parse(url.parse(req.url).query).since, 10);
+
+  channel.query(since, function (messages) {
+    if (session) session.poke();
+    res.simpleJSON(200, { messages: messages, rss: mem.rss });
+  });
+});
+
+fu.get("/send", function(req, res) {
+    var id = qs.parse(url.parse(req.url).query).id;
+    var bsp = qs.parse(url.parse(req.url).query).bsp;
+    var session = sessions[id];
+    if (!session) {
+        res.simpleJSON(400, { error: "No such session id" });
+        return;
+    }
+
+    if (bsp) {
+        var spans = qs.parse(url.parse(req.url).query).spans;
+        if (!spans) {
+            res.simpleJSON(400, { error: "Data format illeg" });
+            return;
+        }
+        session.poke();
+        //channel.appendMessage(session.nick, "msg", text);
+    }
+    else {
+        var text = qs.parse(url.parse(req.url).query).text;
+        if (!text) {
+            res.simpleJSON(400, { error: "Data format illeg" });
+            return;
+        }
+        session.poke();
+        channel.appendMessage(session.nick, "msg", text);
+    }
+
+    res.simpleJSON(200, { rss: mem.rss });
+});
